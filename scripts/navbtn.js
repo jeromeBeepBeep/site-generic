4bb6478884fd57e3036eddb132893aa3b077c135
jerome <jeromepoirette@gmail.com>
2026-05-24 17:59:45 +0200

    mise en forme du panneau de navigation latéral

--- scripts/navbtn.js
+++ scripts/navbtn.js
@@ -1,5 +1,9 @@
-document.getElementById('menu-toggle')
 
+const navPanel = document.getElementById('nav-panel');
+const menuToggle = document.getElementById('menu-toggle');
+
+menuToggle
 .addEventListener('click', function(){
   document.body.classList.toggle('nav-open');
+  navPanel.classList.toggle('is-open');
 });
